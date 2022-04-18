8d31b8d4402e2813cf59339030f91de211b5cc56
Jongok Yoon <bak01076@gmail.com>
2022-04-18 17:55:40 +0200

    Header done

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
 import App from "./App";
-import { createGlobalStyle } from "styled-components";
+import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
 
 const GlobalStyle = createGlobalStyle`
@@ -71,7 +72,10 @@ a {
 }
 `;
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <RecoilRoot>
       <ThemeProvider theme={theme}>
@@ -79,6 +83,10 @@ ReactDOM.render(
         <App />
       </ThemeProvider>
     </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
